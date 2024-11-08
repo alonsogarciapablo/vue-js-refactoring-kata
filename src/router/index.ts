5b5094aa74b322ad9315b5b45e86b1bff5440b2a
Pablo Alonso García <pabloa@empathy.co>
2024-11-08 23:41:24 +0100

    Tinderella

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SignupView from '../views/signup-view.vue'
+import SignupView from '../views/SignupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
