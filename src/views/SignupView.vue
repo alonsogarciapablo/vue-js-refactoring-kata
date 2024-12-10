bacc20e845693550cb21bd2377fc39ec23ea6f93
Pablo Alonso García <pabloa@empathy.co>
2024-12-10 11:36:20 +0100

    Extract User class

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -2,62 +2,7 @@
   import { ref } from 'vue'
   import encrypt from '../utils/encrypt'
   import InMemoryUsersRepository from '../infrastructure/persistence/in-memory-users-repository'
-
-  class User {
-    name: string
-    email: string
-    birthDate: string
-    encryptedPassword: string
-
-    constructor(attrs: {
-      name: string
-      email: string
-      birthDate: string
-      encryptedPassword: string
-    }) {
-      this.name = attrs.name
-      this.email = attrs.email
-      this.birthDate = attrs.birthDate
-      this.encryptedPassword = attrs.encryptedPassword
-    }
-
-    validate(): Array<string> {
-      const errors = []
-      if (!this.name) {
-        errors.push('Name cannot be blank')
-      }
-
-      if (this.email) {
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-        if (!emailRegex.test(email.value)) {
-          errors.push('Email is not valid')
-        }
-      } else {
-        errors.push('Email cannot be blank')
-      }
-
-      if (!this.birthDate) {
-        errors.push('Birthday cannot be blank')
-      }
-      if (this.birthDate && this.calculateAge() < 18) {
-        errors.push('You must be older than 18')
-      }
-      return errors
-    }
-
-    calculateAge() {
-      const today = new Date()
-      const birth = new Date(this.birthDate)
-      let age = today.getFullYear() - birth.getFullYear()
-      const monthDifference = today.getMonth() - birth.getMonth()
-
-      if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birth.getDate())) {
-        age--
-      }
-
-      return age
-    }
-  }
+  import User from '../domain/models/user'
 
   const inMemoryUsersRepository = InMemoryUsersRepository.getInstance()
   const name = ref('')
